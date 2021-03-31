6c4257e90cb755ca44ca8a0c9d333267b0edbb00
sarvar <sarvarlapasov2102@gmail.com>
2021-03-31 12:41:00 +0500

    Birinchi qadam

---
+++ main.js
@@ -0,0 +1,18 @@
+const modal = document.getElementById('simpleModal');
+
+const modalBtn = document.getElementById('modalBtn');
+
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+modalBtn.addEventListener('click', openModal);
+
+closeBtn.addEventListener('click', closeModal);
+
+function openModal(){
+  modal.style.display = 'block';
+}
+
+
+function closeModal(){
+  modal.style.display = 'none';
+}
